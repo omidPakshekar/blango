772c61805a8a63e167ecb566911f440d4aa78050
omid estalkhi <omid.pakshekar1378@gmail.com>
2022-07-09 19:22:00 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -199,3 +199,14 @@ class Dev(Configuration):
 
     # SIGNUP - EMAIL ACTIVATION 
     ACCOUNT_ACTIVATION_DAYS = 7
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
+    }
